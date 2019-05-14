248b24febc873549db5ba364c0726f74dbc3c52c
Deeban-Sakkaravarthy <41464487+Deeban-Sakkaravarthy@users.noreply.github.com>
2019-05-14 15:46:43 +0530

    correction

--- JS/contact-us.js
+++ JS/contact-us.js
@@ -2,10 +2,8 @@ function validateEmail(email) {
   var reEmail = /^(?:[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+\.)*[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+@(?:(?:(?:[a-zA-Z0-9](?:[a-zA-Z0-9\-](?!\.)){0,61}[a-zA-Z0-9]?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9\-](?!$)){0,61}[a-zA-Z0-9]?)|(?:\[(?:(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\.){3}(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\]))$/;
 
   if(!email.match(reEmail)) {
-    alert("Invalid email address");
-    return false;
+      alert("Invalid email address");
+      return false;
   }
-
   return true;
-
 }
